e5773941a22c58e596142670e20e3a519972def7
repeating <fadi.younes.syr@gmail.com>
2022-10-26 00:41:30 +0300

    Add setup configuration

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'SeCreepy',
+  packages = ['SeCreepy'],
+  version = '0.1.0',
+  license='MIT',
+  description = 'A wrapper for undetected chromedriver that works perfectly for long-running scrapping',
+  author_email = 'fadi.younes.syr@gmail.com',
+  url = 'https://github.com/repeating/secreepy',
+  download_url = 'https://github.com/repeating/secreepy/archive/v0.1.0.tar.gz',
+  keywords = ['Selenium', 'undetected chromedriver', 'Web Scrapping'],
+  install_requires=[            # I get to this in a second
+          'selenium',
+          'undetected-chromedriver',
+          'urllib3',
+          'psutil'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
