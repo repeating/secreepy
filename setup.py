0299e1dec60103e29e2b9d2cd04a7423b8228630
repeating <fadi.younes.syr@gmail.com>
2022-10-27 17:36:46 +0300

    Update setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'SeCreepy',
-  packages = ['secreepy'],
-  version = '0.3.0',
+  name='SeCreepy',
+  packages=['secreepy'],
+  version='0.4.0',
   license='MIT',
-  description = 'A wrapper for undetected chromedriver that works perfectly for long-running scrapping',
-  author_email = 'fadi.younes.syr@gmail.com',
-  url = 'https://github.com/repeating/secreepy',
-  download_url = 'https://github.com/repeating/secreepy/archive/v0.3.0.tar.gz',
-  keywords = ['Selenium', 'undetected chromedriver', 'Web Scrapping'],
-  install_requires=[            # I get to this in a second
+  description='A wrapper for undetected chromedriver that works perfectly for long-running scrapping',
+  author_email='fadi.younes.syr@gmail.com',
+  url='https://github.com/repeating/secreepy',
+  download_url='https://github.com/repeating/secreepy/archive/v0.4.0.tar.gz',
+  keywords=['Selenium', 'undetected chromedriver', 'Web Scrapping'],
+  install_requires=[
           'selenium',
           'undetected-chromedriver',
           'urllib3',
@@ -17,10 +17,10 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
