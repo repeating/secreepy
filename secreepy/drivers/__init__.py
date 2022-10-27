b6da0bd9aeaae84428779dccdab413f711bb0a33
repeating <fadi.younes.syr@gmail.com>
2022-10-27 17:36:31 +0300

    Add Chrome Driver

--- secreepy/drivers/__init__.py
+++ secreepy/drivers/__init__.py
@@ -1,3 +1,4 @@
 from .driver import Driver
 from .undetected_chrome_driver import UndetectedChromeDriver
 from .firefox_driver import FirefoxDriver
+from .chrome_driver import ChromeDriver
---
+++ secreepy/drivers/chrome_driver.py
@@ -0,0 +1,24 @@
+from . import Driver
+from selenium import webdriver
+import os
+
+
+class ChromeDriver(Driver):
+    def _get_driver(self):
+        self.timeout = self.timeout_backup
+        options = webdriver.ChromeOptions()
+        options.add_argument('--disable-browser-side-navigation')
+        if self.headless:
+            options.add_argument('--headless')
+        if self.profile:
+            os.makedirs(self.profile, exist_ok=True)
+            options.add_argument('--profile')
+            options.add_argument(self.profile)
+        driver = webdriver.Chrome(options=options)
+        driver.set_page_load_timeout(self.timeout)
+        driver.set_script_timeout(self.timeout)
+        driver.implicitly_wait = self.timeout
+        main_page = driver.window_handles[0]
+        driver.switch_to.window(main_page)
+        driver.maximize_window()
+        return driver
