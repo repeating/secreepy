4cf3fd54cbe85273b826b18c281ae4e167eaa68c
repeating <fadi.younes.syr@gmail.com>
2022-11-15 15:32:17 +0300

    keep timeout decorator only on important functions

--- secreepy/drivers/__init__.py
+++ secreepy/drivers/__init__.py
@@ -1,4 +1,4 @@
-from .driver import Driver
+from .driver import Driver, DRIVER_TIMEOUT
 from .undetected_chrome_driver import UndetectedChromeDriver
 from .firefox_driver import FirefoxDriver
 from .chrome_driver import ChromeDriver
